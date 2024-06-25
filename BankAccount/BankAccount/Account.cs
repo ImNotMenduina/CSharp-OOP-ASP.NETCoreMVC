f261bae48fd529d9c2e4b165043f98548687c0be
Lucas Menduiña <100011745+ImNotMenduina@users.noreply.github.com>
2024-06-25 17:08:10 -0300

    add: bank account project oop charp

---
+++ BankAccount/BankAccount/Account.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    internal class Account
+    {
+        public int AccNumber { get; private set; }
+        public string ClientName { get;  set; }
+        public double Amount { get; private set; }
+
+        public Account(int accNumber, string clientName)
+        {
+            AccNumber = accNumber;
+            ClientName = clientName;
+        }
+
+        public Account(int accNumber, string clientName, double amount) : this(accNumber, clientName)
+        {
+            Amount = amount;
+        }
+
+        public void Withdraw(double withdrawAmount)
+        {
+            if (withdrawAmount <= Amount) {
+                Amount -= withdrawAmount + 5.00;
+            }
+        } 
+
+        public void Deposit(double depositAmount)
+        {
+            Amount += depositAmount;
+        }
+
+        public override string ToString()
+        {
+            return "Conta "
+                + AccNumber
+                + ", Titular: "
+                + ClientName
+                + ", Saldo: $"
+                + Amount.ToString("F2", CultureInfo.InvariantCulture)
+                + "\n";
+        }
+
+    }
+}
---
+++ BankAccount/BankAccount/Program.cs
@@ -0,0 +1,46 @@
+﻿using BankAccount;
+using System;
+using System.Globalization;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre com o número da conta: ");
+            int accNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre o titular da conta: ");
+            string client = Console.ReadLine();
+
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            string op = Console.ReadLine();
+
+            Account acc;
+
+            if (op == "s" || op == "S")
+            {
+                Console.Write("Entre o valor do depósito inicial: ");
+                double initialDeposit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                 acc = new(accNumber, client, initialDeposit);
+            }
+            else
+            {
+                 acc = new(accNumber, client);
+            }
+
+            Console.WriteLine($"\nDados da conta:\n{acc}");
+
+            Console.Write("Entre com um valor para depósito: ");
+            acc.Deposit(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+            Console.WriteLine($"Dados da conta atualizados:\n{acc}");
+
+            Console.Write("Entre um valor para saque: ");
+            acc.Withdraw(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+            Console.WriteLine($"Dados da conta atualizados:\n{acc}");
+
+        }
+    }
+}

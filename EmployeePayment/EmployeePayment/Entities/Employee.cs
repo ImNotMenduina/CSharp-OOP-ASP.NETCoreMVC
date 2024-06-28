cff885066d58a74d337a7b5ec46f276a9c38b397
Lucas Menduiña <100011745+ImNotMenduina@users.noreply.github.com>
2024-06-28 01:08:34 -0300

    add: Employee and OutsourcedEmployee classes and it's getters and setters

---
+++ EmployeePayment/EmployeePayment/Entities/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+
+namespace EmployeePayment.Entities
+{
+    internal class Employee
+    {
+        protected string _name;
+        protected int _hours;
+        protected double _valuePerHour;
+
+        public Employee() { }
+
+        public Employee(string name, int hours, double valuePerHour)
+        {
+            this._name = name;
+            this._hours = hours;
+            this._valuePerHour = valuePerHour;
+        }
+
+        public string Name { get { return _name; } }
+        public int Hours { get { return _hours; } }
+        public double ValuePerHour {  get { return _valuePerHour; } }
+        public override string ToString()
+        {
+            return "Name: "
+                + _name
+                + ", Hours: "
+                + _hours
+                + ", Value/Hour: "
+                + _valuePerHour.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ EmployeePayment/EmployeePayment/Entities/OutsourcedEmployee.cs
@@ -0,0 +1,13 @@
+﻿namespace EmployeePayment.Entities
+{
+    internal class OutsourcedEmployee : Employee
+    {
+        public double AdditionalCharge { get; set; }
+
+        public OutsourcedEmployee(string name, int hours, double valuePerHour, double additionalCharge)
+        : base(name, hours, valuePerHour)
+        { 
+            AdditionalCharge = additionalCharge;
+        }
+    }
+}

ec160e3ec2f795944bf3a4d152ae884f867c8dd4
Lucas Menduiña <100011745+ImNotMenduina@users.noreply.github.com>
2024-06-28 02:39:06 -0300

    add: program logic

--- EmployeePayment/EmployeePayment/Program.cs
+++ EmployeePayment/EmployeePayment/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using EmployeePayment.Entities;
+using System.Globalization;
+using System;
 
 namespace MyApp // Note: actual namespace depends on the project name.
 {
@@ -6,7 +8,59 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Enter the number of employees: ");
+            int n = int.Parse(Console.ReadLine());
+
+            List<Employee> list = new List<Employee>();
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Employee #{i} data: ");
+
+                Console.Write("Outsourced (y/n)");
+                char op = char.Parse(Console.ReadLine());
+
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+
+                Console.Write("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if (op == 'y' || op == 'Y')
+                {
+                    Console.Write("Additional Charge: ");
+                    double additionalCharge = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    list.Add(new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge));
+                }
+                else if (op == 'n' || op == 'N')
+                {
+                    list.Add(new Employee(name, hours, valuePerHour));
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Option");
+                    break;
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("PAYMENTS: ");
+            foreach (Employee emp in list)
+            {
+                if (emp is OutsourcedEmployee)
+                {
+                    OutsourcedEmployee outEmp = (OutsourcedEmployee)emp;
+                    Console.WriteLine($"{outEmp.Name} - $ {outEmp.payment().ToString("F2", CultureInfo.InvariantCulture)}");
+                }
+                else
+                {
+                    Console.WriteLine($"{emp.Name} - $ {emp.payment().ToString("F2", CultureInfo.InvariantCulture)}");
+                }
+            }
         }
     }
 }

4224f1e636467c5fe620f3d07736db92b9d1fba6
Lucas Menduiña <100011745+ImNotMenduina@users.noreply.github.com>
2024-07-01 02:23:16 -0300

    add: RentalService as a service to precess invoice

---
+++ CarRentalInvoice/CarRentalInvoice/Services/RentalService.cs
@@ -0,0 +1,37 @@
+﻿using CarRentalInvoice.Entities;
+using System;
+
+namespace CarRentalInvoice.Services
+{
+    internal class RentalService
+    {
+        public double PricePerHour { get; private set; }
+        public double PricePerDay { get; private set; }
+
+        private BrazilTaxService _brazilTextService = new BrazilTaxService();
+
+        public RentalService(double pricePerHour, double pricePerDay)
+        {
+            PricePerHour = pricePerHour; PricePerDay = pricePerDay;
+        }
+
+        public void ProcessInvoice(CarRental carRental)
+        {
+            TimeSpan duration = carRental.Finish.Subtract(carRental.Start);
+
+            double basicPayment = 0.0;
+            if (duration.TotalHours <= 12.0)
+            {
+                basicPayment = PricePerHour * Math.Ceiling(duration.TotalHours);
+            }
+            else
+            {
+                basicPayment = PricePerDay * Math.Ceiling(duration.TotalDays);
+            }
+
+            double tax = _brazilTextService.Tax(basicPayment);
+
+            carRental.Invoice = new Invoice(basicPayment, tax);
+        }
+    }
+}
